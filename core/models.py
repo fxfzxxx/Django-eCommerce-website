d0151ebff65e2dd0b482859b8ee97e00ffdf0952
fxfzxxx <aaron1995228@gmail.com>
2020-10-09 16:20:20 +0800

    first commit of project, not sure about the brunch

--- core/models.py
+++ core/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+LABEL_CHOICES = (
+    ('P', ' primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
+
+CATEGORY_CHOICES = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport wear'),
+    ('OW', 'Outwear')
+)
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    category = models.CharField(default='S', choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(default='P', choices=LABEL_CHOICES, max_length=1)
+
+    def __str__(self):
+        return self.title
+
+
+class OrderItems(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItems)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Item
+from django.views.generic import ListView, DeleteView
 
-# Create your views here.
+
+def products(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, "products.html", context)
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = "home-page.html"
+
+#   def home(request):
+#        context = {
+#            'items': Item.objects.all()
+#        }
+#        return render(request, "home-page.html", context)
+
+
+#def home(request):
+    #return render(request, "checkout.html")
